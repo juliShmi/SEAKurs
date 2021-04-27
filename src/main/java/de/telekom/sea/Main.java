af521666150d0826af0642a91e0b5b3d73511fa5
juliShmi <juliashmitko@gmail.com>
2021-04-27 11:47:07 +0300

    Package import

---
+++ src/main/java/de/telekom/sea/Main.java
@@ -0,0 +1,13 @@
+package de.telekom.sea;
+
+import de.telekom.sea.seminar.SeminarApp;
+
+public class Main {
+
+	public static void main(String[] args) {
+		SeminarApp app = new SeminarApp();
+		app.run(args);
+
+	}
+
+}
---
+++ src/main/java/de/telekom/sea/seminar/SeminarApp.java
@@ -0,0 +1,8 @@
+package de.telekom.sea.seminar;
+
+public class SeminarApp {
+	public void run(String[] args) {
+		System.out.println("Hello Maven");
+	}
+
+}
